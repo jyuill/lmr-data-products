54b54af8905719ad07532a153ca9f8de535f99a7
John Yuill <john@fig4.com>
2024-08-05 16:35:50 -0700

    getting started

---
+++ lmr-data/query.R
@@ -0,0 +1,28 @@
+
+library(tidyverse) 
+library(RMariaDB) ## best way to access MySQL from R
+library(dotenv)
+
+# create a .env file in the root directory of the project
+# and add the following lines
+# ENDPT="...rds.amazonaws.com"
+# APWD="A...KOCX"
+# APORT=3..6  
+
+## Load the .env file
+readRenviron(".env")
+
+## Load the environment variables
+endpt <- Sys.getenv("ENDPT")
+apwd <- Sys.getenv("APWD")
+aport <- as.numeric(Sys.getenv("APORT"))
+
+con_aws <- dbConnect(RMariaDB::MariaDB(),
+                     host=endpt,
+                     user='admin',
+                     password=apwd,
+                     port=aport)
+# main query - all the data
+lmr_data <- dbGetQuery(con_aws, "SELECT * FROM bcbg.tblLDB_lmr lmr
+                           LEFT JOIN bcbg.tblLDB_quarter qtr ON lmr.fy_qtr=qtr.fy_qtr;")
+dbDisconnect(con_aws)
